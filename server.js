83d6ea9ddb829dce5d9a7c7d144ec5bdb33341e9
Julien <julien.ramirez@gmail.com>
2023-03-20 12:04:50 +0100

    middle project

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const express = require('express')
 const PORT = 5000 || process.env.PORT
-const app = express()
+const path = require('path')
+const http = require('http')
+const formatMessage = require('./utils/messages')
+const socketio = require('socket.io')
+const devBotName = 'DevCord Bot'
 
-app.listen(PORT, () => {
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Run when client connects
+io.on('connection', socket => {
+
+    //Welcome current user
+    //Emit émet seulement à l'utilisateur
+    //io.emit émet à tous les utilisateurs
+    socket.emit('message', formatMessage(devBotName,'Welcome to DevCord'))
+
+    //Broadcast when a user connects
+    //Broadcast: émet a tous sauf à l'utilisateur
+    socket.broadcast.emit('message', formatMessage(devBotName,'A user has joined the chat'))
+
+    //Runs when client disconnects
+    //Doit se trouver dans on.connect
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage( devBotName,'A user has left the chat'))
+    })
+
+    //Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', formatMessage( 'USER', msg))
+    })
+
+})
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
