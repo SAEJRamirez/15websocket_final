9dddf86576a6a4afa8aaf71dc1e01a7104930085
Julien <julien.ramirez@gmail.com>
2023-03-20 23:47:27 +0100

    last commit final project

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,23 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
 const socket = io()
+
+//Get username and room from url
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+//Join chatroom
+socket.emit('joinRoom', {username, room})
+
+//Get room and users
+socket.on('roomUsers', ({ room, users}) => {
+    outputRoomName(room)
+    outputUsers(users)
+
+})
 
 //Message from server
 socket.on('message', message => {
@@ -38,4 +55,16 @@ function outputMessage(message) {
                         </p>
                     `
     document.querySelector('.chat-messages').appendChild(div)
+}
+
+//Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room
+}
+
+//Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = 5000 || process.env.PORT
 const path = require('path')
 const http = require('http')
 const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
 const socketio = require('socket.io')
 const devBotName = 'DevCord Bot'
 
@@ -15,25 +16,48 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //Run when client connects
 io.on('connection', socket => {
+    socket.on('joinRoom', ({username, room}) => {
 
-    //Welcome current user
-    //Emit émet seulement à l'utilisateur
-    //io.emit émet à tous les utilisateurs
-    socket.emit('message', formatMessage(devBotName,'Welcome to DevCord'))
+        //Join room
+        const user = userJoin(socket.id, username, room)
+        socket.join(user.room)
 
-    //Broadcast when a user connects
-    //Broadcast: émet a tous sauf à l'utilisateur
-    socket.broadcast.emit('message', formatMessage(devBotName,'A user has joined the chat'))
+        //Welcome current user
+        //Emit émet seulement à l'utilisateur
+        //io.emit émet à tous les utilisateurs
+        socket.emit('message', formatMessage(devBotName,'Welcome to DevCord'))
+
+        //Broadcast when a user connects
+        //Broadcast: émet a tous sauf à l'utilisateur
+        socket.broadcast.to(user.room).emit('message', formatMessage(devBotName,`${user.username} has joined the chat`))
+
+        //Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+    })
 
     //Runs when client disconnects
     //Doit se trouver dans on.connect
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage( devBotName,'A user has left the chat'))
+        const user = userLeave(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('message', formatMessage( devBotName, `${user.username} has left the chat`))
+            //Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
     })
 
     //Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', formatMessage( 'USER', msg))
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage( `${user.username}`, msg))
     })
 
 })
